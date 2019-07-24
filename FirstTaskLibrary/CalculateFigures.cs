87f65bcdce5ad5ff7facdd37bdae32d2430d8d6e
anb <borodina-a.b@yandex.ru>
2019-07-24 17:25:25 +0300

    Add CalculateFigure class for calculating total area and total perimeter of figures

---
+++ FirstTaskLibrary/CalculateFigures.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace FirstTaskLibrary
+{
+    public class CalculateFigures
+    {
+        public double TotalPerimeter(List<Figure> figures)
+        {
+           return figures.Sum(item => item.Perimeter());
+        }
+
+        public double TotalArea(List<Figure> figures)
+        {
+           return figures.Sum(item => item.Area());
+
+        }
+    }
+}
