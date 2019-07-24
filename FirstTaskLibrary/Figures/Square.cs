6d7d09d8626f11b86d353f82dc6f4f01aac03874
anb <borodina-a.b@yandex.ru>
2019-07-24 18:02:20 +0300

    Add square class

---
+++ FirstTaskLibrary/Figures/Square.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTaskLibrary.Figures
+{
+    public class Square : Rectangle
+    {
+        //public double Side { get; set; }
+        public Square(double side)
+            : base(side, side)
+        {
+        }
+    }
+}
