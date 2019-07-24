aec8bcee20d2be867b83892acd6eacf3d77f53a2
anb <borodina-a.b@yandex.ru>
2019-07-24 17:09:58 +0300

    Add Circle class

---
+++ FirstTaskLibrary/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTaskLibrary
+{
+    public class Circle : Figure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+            : base()
+        {
+            this.Radius = radius;
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
+
