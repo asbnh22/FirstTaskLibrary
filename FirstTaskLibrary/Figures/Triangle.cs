67096c4f5215643585bdc70c7fc6953db326b944
anb <borodina-a.b@yandex.ru>
2019-07-24 18:03:32 +0300

    Add Triangle class

---
+++ FirstTaskLibrary/Figures/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTaskLibrary.Figures
+{
+    public class Triangle : Figure
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+            : base()
+        {
+            this.SideA = sideA;
+            this.SideB = sideB;
+            this.SideC = sideC;
+        }
+
+        public override double Area()
+        {
+            double semiPerimeter = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - SideA) * (semiPerimeter - SideB) * (semiPerimeter - SideC));
+        }
+
+        public override double Perimeter()
+        {
+            return SideA + SideB + SideC;
+        }
+    }
+}
